51abf1aa6b8df7ebea17f04c6099f7e49351632c
benwatsonuk <hello@benwatson.eu>
2026-01-16 08:58:47 +0000

    Test progress

--- src/functions/flows/getFlows.ts
+++ src/functions/flows/getFlows.ts
@@ -1,12 +1,17 @@
 import { PageFlowArray, PagesArray, StagesArray } from "../../types"
-import { validatePageFlowArray, validatePagesArray } from "../../validate"
+import { validatePageFlowArray, validatePagesArray, validateStagesArray } from "../../validate"
 
 export const getFlows = (flows: PageFlowArray, pages: PagesArray, stages?: StagesArray) => {
   const validatedPageFlows = validatePageFlowArray(flows)
-  validatePagesArray(pages)
-  return validatedPageFlows
+  const validatedPages = validatePagesArray(pages)
+  if (stages) {
+    const validatedStages = validateStagesArray(stages)
+    return mapPagesToFlow(validatedPageFlows, validatedPages, validatedStages)
+  }
+  return mapPagesToFlow(validatedPageFlows, validatedPages)
 }
 
-const mapPagesToFlow = (flow: PageFlowArray, pages: PageFlowArray, stages: StagesArray) => {
+const mapPagesToFlow = (flow: PageFlowArray, pages: PagesArray, stages?: StagesArray) => {
   // To be implemented later
+  return flow
 }
--- test/getFlows.test.js
+++ test/getFlows.test.js
@@ -3,13 +3,20 @@ const expect = require('chai').expect
 const getFlows = require('../src/functions/flows/getFlows.ts')
 const {validFlow, invalidFlow} = require('./data/flows.ts')
 const {validPages, invalidPages} = require('./data/pages.ts')
+const {validStages, invalidStages} = require('./data/stages.ts')
 const outputs = require('./data/outputs.ts')
 
 describe('Basic getFlows functions', () => {
     describe('getFlows', () => {
-      it('should return an array of flows when VALID flows and pages JSON is provided', () => {
+      it('should return an array of flows when VALID flows and pages JSON is provided - no stages object is provided, this is optional so should pass', () => {
         const result = getFlows.getFlows(validFlow, validPages)
         const output = outputs.getPageFlowTestA.output
+        expect(result).to.eql(output)
+      })
+
+      it('should return an array of flows when VALID flows, pages and stages JSON is provided', () => {
+        const result = getFlows.getFlows(validFlow, validPages, validStages)
+        const output = outputs.getPageFlowTestB.output
         expect(result).to.eql(output)
       })
 
@@ -19,6 +26,18 @@ describe('Basic getFlows functions', () => {
 
       it('should return a useful error when VALID flows and INVALID pages JSON is provided', () => {
         expect(() => getFlows.getFlows(validFlow, invalidPages)).to.throw("Invalid array of PAGES passed to govuk-pages-plugin - please check the documentation to ensure the JSON schema you are passing matches what is expected")
+      })
+
+      it('should return a useful error when VALID flows and pages and INVALID stages JSON is provided', () => {
+        expect(() => getFlows.getFlows(validFlow, validPages, invalidStages)).to.throw("Invalid array of STAGES passed to govuk-pages-plugin - please check the documentation to ensure the JSON schema you are passing matches what is expected")
+      })
+
+      it('should return a useful error when VALID flows and stages and INVALID pages JSON is provided', () => {
+        expect(() => getFlows.getFlows(validFlow, invalidPages, validStages)).to.throw("Invalid array of PAGES passed to govuk-pages-plugin - please check the documentation to ensure the JSON schema you are passing matches what is expected")
+      })
+
+      it('should return a useful error when VALID pages and stages and INVALID flows JSON is provided', () => {
+        expect(() => getFlows.getFlows(invalidFlow, validPages, validStages)).to.throw("Invalid array of FLOWS passed to govuk-pages-plugin - please check the documentation to ensure the JSON schema you are passing matches what is expected")
       })
     })
   })
